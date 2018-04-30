216b14d51826af1292cd27848f1cce7b36da22fc
Ryan Sherry <rsherrycomputers@gmail.com>
2018-04-30 07:24:44 -0700

    added second activity and intents

---
+++ app/src/main/java/com/teamtreehouse/interactivestory/StoryActivity.java
@@ -0,0 +1,28 @@
+package com.teamtreehouse.interactivestory;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+
+public class StoryActivity extends AppCompatActivity {
+
+    public static final String TAG = StoryActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+        Intent intent = getIntent();
+        Resources resources = getResources();
+        String key = resources.getString(R.string.key_name);
+        String name = intent.getStringExtra(key);
+        if (name == null || name.isEmpty()) {
+            name = "Friend";
+        }
+        Log.d(TAG, name);
+    }
+}
